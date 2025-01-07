a815d444052ed83c863fbcdcea5cba47ff61a4dd
kej <dossiba0604@gmail.com>
2025-01-07 13:46:14 +0900

    chore : primary color 임시 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: '006BFF', // 임시 작성
+      },
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
       },
