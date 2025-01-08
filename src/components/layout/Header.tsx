47001e0c6f6c11cbcae3c8a25edc8b50e3914eb8
zzi_eun <je98710@gmail.com>
2025-01-08 20:37:22 +0900

    JL8-26 chore: 반응형 네이게이션 메뉴바 & 햄버거 메뉴바 초기 구현

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import Hamburger from './Hamburger'
+import Navbar from './Navdar'
+
+const Header = () => {
+  return (
+    <>
+      <Navbar />
+      <Hamburger />
+    </>
+  )
+}
+
+export default Header
