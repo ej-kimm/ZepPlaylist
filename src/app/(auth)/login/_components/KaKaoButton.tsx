8b9b7f56a50ff94c32d0db96dbf09edb2bfc9736
Hobin-joo <wnghqls12@naver.com>
2025-01-08 18:27:02 +0900

    JL8-46 feat:스포티파이 소셜로그인 뼈대 및 구글 로그인 뼈대

--- src/app/(auth)/login/_components/KaKaoButton.tsx
+++ src/app/(auth)/login/_components/KaKaoButton.tsx
@@ -8,21 +8,42 @@ const KaKaoButton = () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'kakao',
       options: {
-        redirectTo: 'http://localhost:3000/api/login',
+        redirectTo: 'http://localhost:3000/api/kakao',
       },
     })
     if (data?.url) {
       redirect(data.url)
     } else if (error) console.error('====카카오 오류', error.message)
   }
+  const signInWithSpotify = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'spotify',
+      options: {
+        redirectTo: 'http://localhost:3000/api/spotify',
+      },
+    })
+
+    if (data.url) {
+      redirect(data.url) // use the redirect API for your server framework
+    } else if (error) console.error('====스포티파이이 오류', error.message)
+  }
   return (
-    <button
-      className="flex items-center space-x-2 rounded bg-yellow-500 px-4 py-2 text-white transition-all hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
-      type="button"
-      onClick={signInWithKakao}
-    >
-      <span>카카오</span>
-    </button>
+    <form>
+      <button
+        className="flex items-center space-x-2 rounded bg-yellow-500 px-4 py-2 text-white transition-all hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+        type="button"
+        onClick={signInWithKakao}
+      >
+        <span>카카오</span>
+      </button>
+      <button
+        className="flex items-center space-x-2 rounded bg-blue-500 px-4 py-2 text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        type="button"
+        onClick={signInWithSpotify}
+      >
+        <span>스포티파이</span>
+      </button>
+    </form>
   )
 }
 
--- src/app/(auth)/login/_components/SocialForm.tsx
+++ src/app/(auth)/login/_components/SocialForm.tsx
@@ -5,12 +5,6 @@ const SocialForm = async () => {
     <div className="mx-auto mt-8 max-w-md">
       <h2 className="mb-4 text-lg font-bold">간편로그인</h2>
       <form className="flex space-x-4">
-        <button
-          className="flex items-center space-x-2 rounded bg-blue-500 px-4 py-2 text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          type="button"
-        >
-          <span>스포티파이</span>
-        </button>
         <KaKaoButton />
         <button
           className="flex items-center space-x-2 rounded bg-red-500 px-4 py-2 text-white transition-all hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"
