22820cac70358891a3809e9f16f9f7a67feb4ba4
kej <dossiba0604@gmail.com>
2025-01-09 10:57:01 +0900

    git commit -m "JL8-27 chore : spotify sdk 관련로직 모두 삭제"

--- src/api/spotifyToken.ts
+++ src/api/spotifyToken.ts
@@ -18,18 +18,3 @@ export const fetchSpotifyToken = async () => {
   const { access_token: token } = await res.json()
   return token
 }
-
-export const generateSpotifyAuthURL = async () => {
-  const scope =
-    'user-read-private user-read-email user-read-playback-state user-modify-playback-state streaming'
-
-  const auth_query_parameters = new URLSearchParams({
-    response_type: 'code',
-    client_id: process.env.SPOTIFY_CLIENT_ID as string,
-    scope: scope,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI as string,
-  })
-  const authURL = `https://accounts.spotify.com/authorize?${auth_query_parameters.toString()}`
-
-  return authURL
-}
