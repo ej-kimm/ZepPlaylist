0b3ce35ab11cfa67dcfc68f62f376146d97442f7
kej <dossiba0604@gmail.com>
2025-01-09 11:45:56 +0900

    git commit -m "JL8-27 feat : preview URL 반환하는 함수"

--- src/api/spotifyToken.ts
+++ src/api/spotifyToken.ts
@@ -18,3 +18,39 @@ export const fetchSpotifyToken = async () => {
   const { access_token: token } = await res.json()
   return token
 }
+
+export const fetchPreviewUrl = async (trackId: string) => {
+  try {
+    const res = await fetch(`https://open.spotify.com/embed/track/${trackId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const htmlContent = await res.text()
+
+    // <script> 태그 중 id가 __NEXT_DATA__인 태그에서 JSON 데이터를 추출
+    const regex =
+      /<script id="__NEXT_DATA__" type="application\/json">([\s\S]+?)<\/script>/
+    const match = htmlContent.match(regex)
+
+    if (match) {
+      // 매칭된 content에서 JSON 데이터를 파싱
+      const jsonData = JSON.parse(match[1])
+
+      // audio preview가 포함되어 있는지 체크하고 있으면 url 반환
+      if (jsonData?.props?.pageProps?.state?.data?.entity?.audioPreview?.url) {
+        return jsonData.props.pageProps.state.data.entity.audioPreview?.url
+      } else {
+        console.log('No track list found or no audio preview available.')
+        return []
+      }
+    } else {
+      console.log('No JSON data found in the page.')
+      return []
+    }
+  } catch (error) {
+    console.error('Error fetching playlist data:', error)
+    return []
+  }
+}
