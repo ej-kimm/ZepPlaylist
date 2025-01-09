cce2e223f65b4aae6230bf9c5af7a8fa06337c07
kej <dossiba0604@gmail.com>
2025-01-09 14:24:31 +0900

    JL8-27 feat : supabase musicId에 따라 음악 재생

--- src/app/music-play/page.tsx
+++ src/app/music-play/page.tsx
@@ -1,9 +1,12 @@
+import { fetchMusicId } from '@/api/supabase'
 import MusicPlayer from './_components/MusicPlayer'
 
-const MusicPlayPage = () => {
+const MusicPlayPage = async () => {
+  const trackIds = await fetchMusicId() // music테이블에서 spotify_id를 모두 갖고옴
+
   return (
     <>
-      <MusicPlayer />
+      <MusicPlayer trackIds={trackIds} />
     </>
   )
 }
