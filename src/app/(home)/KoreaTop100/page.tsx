644b9883a5892c16101610b0b24219d6e3ce6bc6
zzi_eun <je98710@gmail.com>
2025-01-13 19:22:57 +0900

    JL8-26 refator: TOP100List 컨포넌트 리팩토링

--- src/app/(home)/KoreaTop100/page.tsx
+++ src/app/(home)/KoreaTop100/page.tsx
@@ -1,4 +1,5 @@
 import { fetchMelonChart } from '@/api/home/actions'
+import type { MelonChartSong } from '@/types/melonCharts'
 import Top100ChartList from '../_components/Top100ChartList'
 
 export const revalidate = 3600
@@ -7,9 +8,17 @@ const KoreaTop100 = async () => {
   const koreaTop100Chart = await fetchMelonChart()
 
   const koreaTop100ChartList = koreaTop100Chart.response.SONGLIST
+
+  const newData: {
+    isKoreaChart: true
+    list: MelonChartSong[]
+  } = {
+    isKoreaChart: true,
+    list: koreaTop100ChartList,
+  }
   return (
     <div>
-      <Top100ChartList koreaTop100ChartList={koreaTop100ChartList} />
+      <Top100ChartList data={newData} />
     </div>
   )
 }
