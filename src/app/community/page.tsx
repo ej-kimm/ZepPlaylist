f9124131dd5c54450f457ce31b62807361cee861
bj9322 <bj932218@gmail.com>
2025-01-14 11:39:21 +0900

    git commit -m "JL8-15 feat: ssr 방식에서의 로그인한 유저정보 확인"

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,25 +1,60 @@
-import { getPlaylists, getPopularPlaylists } from '@/api/community/playlists';
-import PlaylistSection from '@/app/community/_components/PlaylistSection';
+import { getPlaylists, getPopularPlaylists } from '@/api/community/playlists'
+import PlaylistSection from '@/app/community/_components/PlaylistSection'
+import type { Database } from '@/types/supabase'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
 const CommunityPage = async (): Promise<JSX.Element> => {
-  // 하드코딩된 유저 ID (임시 처리)
-  const userId = '7db22ced-2ee8-45db-8127-f190ff3725ef';
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => cookies().getAll(),
+        setAll: (cookiesToSet) => {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookies().set(name, value, options)
+          })
+        },
+      },
+    },
+  )
 
-  // SSR에서 초기 데이터 로드
-  const playlists = await getPlaylists(userId);
-  const popularPlaylists = await getPopularPlaylists(userId);
+  const { data: session } = await supabase.auth.getSession()
+
+  if (!session || !session.session?.user) {
+    console.error('유저 정보를 가져오는 데 실패했습니다.')
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold text-red-500">
+          로그인된 사용자가 없습니다. 다시 로그인해주세요.
+        </h1>
+      </div>
+    )
+  }
+
+  const userId = session.session.user.id
+
+  const playlists = (await getPlaylists(userId)).map((playlist) => ({
+    ...playlist,
+    description: playlist.description || '',
+  }))
+  const popularPlaylists = (await getPopularPlaylists(userId)).map(
+    (playlist) => ({
+      ...playlist,
+      description: playlist.description || '',
+    }),
+  )
 
   return (
     <div className="p-4">
-      {/* 인기 있는 플레이리스트 섹션 */}
       <h1 className="mb-4 text-2xl font-bold">인기 있는 플레이리스트</h1>
       <PlaylistSection playlists={popularPlaylists} isSwiper userId={userId} />
 
-      {/* 전체 플레이리스트 섹션 */}
       <h1 className="mb-4 mt-8 text-2xl font-bold">전체 플레이리스트</h1>
       <PlaylistSection playlists={playlists} userId={userId} />
     </div>
-  );
-};
+  )
+}
 
-export default CommunityPage;
+export default CommunityPage
