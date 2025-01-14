302fa4e75e3391edd55197e5f7fd433b07795188
Hobin-joo <wnghqls12@naver.com>
2025-01-14 22:45:43 +0900

    JL8-24 feat:공용컴포넌트 타입,프롭스 설정 추가

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -5,12 +5,16 @@ type BottomSheetProps = {
   isOpen: boolean
   onClose: () => void
   children: React.ReactNode
+  height: string
+  maxWidth: string
 }
 
 const BottomSheet: React.FC<BottomSheetProps> = ({
   isOpen,
   onClose,
   children,
+  height = '50%',
+  maxWidth = '500px',
 }) => {
   useEffect(() => {
     const handleEscape = (e: KeyboardEvent) => {
@@ -39,6 +43,13 @@ const BottomSheet: React.FC<BottomSheetProps> = ({
           />
           <motion.div
             className="fixed bottom-0 left-0 right-0 z-50 h-[70%] rounded-t-3xl bg-white p-6 shadow-lg"
+            style={{
+              height,
+              maxWidth,
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
             initial={{ y: '100%' }}
             animate={{ y: '0%' }}
             exit={{ y: '100%' }}
