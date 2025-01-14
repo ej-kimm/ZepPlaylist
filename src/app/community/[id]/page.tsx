c7d478c88fa1147bcb68409c2075fe0a2c39faee
bj9322 <bj932218@gmail.com>
2025-01-14 19:11:17 +0900

    git commit -m "JL8-15 feat: 댓글기능 완료 및 코드 리팩토링"

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,9 +1,28 @@
-import CommunityDetailClient from '@/app/community/_components/CommunityDetailClient';
+import { getCommunityDetail } from '@/api/community/communityDetail'
+import { headers } from 'next/headers'
+import CommentSection from '../_components/CommentSection'
 
-export default function CommunityDetailPage({ params }: { params: { id: string } }) {
+const CommunityDetailPage = async ({
+  params,
+}: {
+  params: { id: string }
+}): Promise<JSX.Element> => {
+  const cookies = headers().get('cookie') || ''
+  const { songs, songCount, comments } = await getCommunityDetail(
+    params.id,
+    cookies,
+  )
+
   return (
     <div className="p-4">
-      <CommunityDetailClient playlistId={params.id} />
+      <CommentSection
+        songs={songs}
+        songCount={songCount}
+        comments={comments}
+        playlistId={params.id}
+      />
     </div>
-  );
+  )
 }
+
+export default CommunityDetailPage
