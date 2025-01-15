aada6d7e30d58e5438cfc08f55ecdc403a51bcb4
kej <dossiba0604@gmail.com>
2025-01-16 02:24:59 +0900

    JL8-27 style : 공통 컬러 색상 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: 'B15EFF',
-        secondary: '9032E8',
+        primary: '#B15EFF',
+        secondary: '#9032E8',
+      },
+      boxShadow: {
+        drop: '0px -4px 4px rgba(0, 0, 0, 0.04)',
       },
       zIndex: {
         header: '10',
+        player: '10',
       },
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
