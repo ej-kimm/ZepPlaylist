71f9e446c43197137ac6dbb4a5e2ccdd6398b08a
zzi_eun <je98710@gmail.com>
2025-01-17 01:34:08 +0900

    JL8-26 feat: 검색 결과 컴포넌트 플레이리스트 담기 기능 추가 및 리팩토링

--- src/app/(home)/_components/SearchResult.tsx
+++ src/app/(home)/_components/SearchResult.tsx
@@ -1,9 +1,8 @@
 import { fetchSearchTracks } from '@/api/home/actions'
-import Image from 'next/image'
+import SearchResultItem from './SearchResultItem'
 
 const SearchResult = async ({ searchParams }: { searchParams: string }) => {
   const searchResult = await fetchSearchTracks(searchParams)
-
   const searchResultList = searchResult!.tracks.items
 
   return (
@@ -11,29 +10,7 @@ const SearchResult = async ({ searchParams }: { searchParams: string }) => {
       <h1 className="mb-3 text-2xl font-bold">{searchParams} 검색 결과</h1>
       <ul>
         {searchResultList.map((item) => (
-          <li
-            className="flex items-center space-x-4 rounded-lg p-3 transition-colors"
-            key={item.id}
-          >
-            <div className="relative flex-shrink-0">
-              <Image
-                src={item.album.images[0].url}
-                alt={item.album.name}
-                width={50}
-                height={50}
-                className="rounded-md"
-                priority
-              />
-            </div>
-            <div className="min-w-0 flex-1">
-              <h3 className="truncate text-base font-medium text-gray-900">
-                {item.name}
-              </h3>
-              <p className="truncate text-sm text-gray-500">
-                {item.artists[0].name}
-              </p>
-            </div>
-          </li>
+          <SearchResultItem item={item} />
         ))}
       </ul>
     </div>
