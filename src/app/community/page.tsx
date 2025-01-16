606ea82ce20fcee8ac8adb5d9e416318475758c7
bj9322 <bj932218@gmail.com>
2025-01-16 16:18:45 +0900

    git commit -m "JL8-15 feat: 리펙토링"

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,40 +1,37 @@
-import { getPlaylists, getPopularPlaylists } from '@/api/community/actions';
-import PlaylistSection from '@/app/community/_components/PlaylistSection';
-import ClientSwiper from '@/components/common/ClientSwiper';
-import KeywordCarouselWrapper from './_components/KeywordCarouselWrapper';
-import type { Database } from '@/types/supabase';
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { getPlaylists, getPopularPlaylists } from '@/api/community/actions'
+import PlaylistSection from '@/app/community/_components/PlaylistSection'
+import ClientSwiper from '@/components/common/ClientSwiper'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import KeywordCarouselWrapper from './_components/KeywordCarouselWrapper'
 
 const CommunityPage = async (): Promise<JSX.Element> => {
-  const supabase = createServerClient<Database>(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll: () => cookies().getAll(),
       },
-    }
-  );
+    },
+  )
 
-  const { data: session } = await supabase.auth.getSession();
+  const { data: session } = await supabase.auth.getSession()
 
   if (!session || !session.session?.user) {
-    console.error('유저 정보를 가져오는 데 실패했습니다.');
+    console.error('유저 정보를 가져오는 데 실패했습니다.')
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold text-red-500">
           로그인된 사용자가 없습니다. 다시 로그인해주세요.
         </h1>
       </div>
-    );
+    )
   }
 
-  const userId = session.session.user.id;
-
-  const allPlaylists = await getPlaylists(userId);
-
-  const popularPlaylists = await getPopularPlaylists(userId);
+  const userId = session.session.user.id
+  const allPlaylists = await getPlaylists(userId)
+  const popularPlaylists = await getPopularPlaylists(userId)
 
   return (
     <div className="p-4">
@@ -42,14 +39,12 @@ const CommunityPage = async (): Promise<JSX.Element> => {
       <ClientSwiper
         items={popularPlaylists.map((playlist) => ({
           id: playlist.id,
-          content: (
-            <PlaylistSection playlists={[playlist]} userId={userId} />
-          ),
+          content: <PlaylistSection playlists={[playlist]} userId={userId} />,
         }))}
       />
       <KeywordCarouselWrapper allPlaylists={allPlaylists} userId={userId} />
     </div>
-  );
-};
+  )
+}
 
-export default CommunityPage;
+export default CommunityPage
