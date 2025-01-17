d979479518fc3231a31364705b3f07587e7364ba
kej <dossiba0604@gmail.com>
2025-01-17 15:38:03 +0900

    JL8-27 style : 가사 작은 창일 때 height 40px

--- src/components/music-play/_components/Lyrics.tsx
+++ src/components/music-play/_components/Lyrics.tsx
@@ -44,7 +44,7 @@ const Lyrics = ({ lyrics, isFullLyrics, onClickLyrics }: LyricsProps) => {
         <p className="caption-1">번역기능은 MVP이후지롱</p>
       ) : (
         <p
-          className={`cursor-pointer overflow-y-scroll ${isFullLyrics ? 'lyrics-full caption-2 relative -left-1 h-[300px] text-left leading-[25px]' : 'lyrics-small caption-1 h-[60px] text-center leading-5'}`}
+          className={`cursor-pointer overflow-y-scroll ${isFullLyrics ? 'lyrics-full caption-2 relative -left-1 h-[300px] text-left leading-[25px]' : 'lyrics-small caption-1 h-[40px] text-center leading-5'}`}
           dangerouslySetInnerHTML={{ __html: lyrics }}
           onClick={onClickLyrics}
         />
