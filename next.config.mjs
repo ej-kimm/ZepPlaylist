97a2b8a186e23c5df30156db947580a14b733b1e
kej <dossiba0604@gmail.com>
2025-01-17 20:30:27 +0900

    chore : svg webpack 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
   images: {
     remotePatterns: [
       {
