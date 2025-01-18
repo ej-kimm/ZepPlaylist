2003c9695b8964eabd592d92c2dbcd977b0bfc54
kej <dossiba0604@gmail.com>
2025-01-19 02:48:47 +0900

    JL8-27 chore : 필요없는 주석 삭제

--- src/components/music-play/_components/AlbumCover.tsx
+++ src/components/music-play/_components/AlbumCover.tsx
@@ -38,31 +38,13 @@ export default function AlbumCover({ musicDetail }: AlbumCoverProps) {
             className="rounded object-cover"
           />
         </div>
-        {/* <div className="rotate-y-180 absolute left-0 top-0 h-full w-full [backface-visibility:hidden]">
-          <Image
-            src={album_cover || '/No cover'}
-            alt={title || 'No Title'}
-            width={266}
-            height={266}
-            className="rounded bg-gradient-to-t from-black/35 to-black/35 object-cover blur-[10px]"
-          />
-          <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-6">
-            <p className="caption-1 text-white">{title}</p>
-            <p className="caption-1 text-white">{artist}</p>
-            <p className="caption-1 text-white">{album?.releaseDate}</p>
-            <p className="caption-1 text-white">{album?.albumName}</p>
-            {genre.genres[0] && (
-              <p className="caption-1 text-white">{genre.genres[0]}</p>
-            )}
-          </div>
-        </div> */}
         <div className="rotate-y-180 absolute left-0 top-0 h-full w-full [backface-visibility:hidden]">
           <Image
             src={album_cover || '/No cover'}
             alt={title || 'No Title'}
             width={266}
             height={266}
-            className="rounded bg-gradient-to-t from-black/80 to-black/80 object-cover blur-[10px]"
+            className="rounded bg-gradient-to-t from-black/35 to-black/35 object-cover blur-[10px]"
           />
           <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-6">
             <p className="caption-1 text-white">{title}</p>
