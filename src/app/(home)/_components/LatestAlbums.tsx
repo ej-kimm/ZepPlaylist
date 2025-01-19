7105a0e6e21ac7b11cde2339da6a2c2ec1b97771
zzi_eun <je98710@gmail.com>
2025-01-19 23:23:12 +0900

    JL8-26 fix: 최신음반 음악재생기능 오류 수정

--- src/app/(home)/_components/LatestAlbums.tsx
+++ src/app/(home)/_components/LatestAlbums.tsx
@@ -1,7 +1,7 @@
 // 최신발매곡 10곡을 슬라이드 형식으로 보여준다 - 1초 랑 넘어가기 버튼 (ISR)
 
 import { fetchNewReleases } from '@/api/home/actions'
-import Image from 'next/image'
+import LatestAlbumItme from './LatestAlbumItem'
 
 // 1시간 간격의 ISR
 export const revalidate = 3600
@@ -16,27 +16,9 @@ const LatestAlbums = async () => {
       <h2 className="my-4">최신 음반</h2>
       <ul className="flex space-x-4 overflow-x-auto">
         {latestAlbumList.map((album) => (
-          <li key={album.id} className="flex-none">
-            <div className="w-32 text-left">
-              <Image
-                src={album.images[0].url}
-                width={100}
-                height={100}
-                alt={album.name}
-                priority
-                className="h-auto w-full rounded-lg shadow-md"
-              />
-              <h3 className="mt-2 truncate text-sm font-semibold">
-                {album.name}
-              </h3>
-              <p className="truncate text-xs text-gray-500">
-                {album.artists[0].name}
-              </p>
-            </div>
-          </li>
+          <LatestAlbumItme album={album} key={album.id} />
         ))}
       </ul>
-      {/* <LatestAlbumItmes latestAlbumList={latestAlbumList} /> */}
     </div>
   )
 }
