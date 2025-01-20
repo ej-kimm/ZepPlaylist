a4b40637707b3f84f75c5d61e535633da7d3974a
zzi_eun <je98710@gmail.com>
2025-01-20 17:21:28 +0900

    JL8-26 chore: 버셀 테스트4

--- src/app/(home)/KoreaTop100/page.tsx
+++ src/app/(home)/KoreaTop100/page.tsx
@@ -1,25 +1,21 @@
-import { fetchMelonChart } from '@/api/home/actions'
-import type { MelonChartSong } from '@/types/melonCharts'
-import Top100ChartList from '../_components/Top100ChartList'
-
 export const revalidate = 3600
 
 const koreaTop100 = async () => {
-  const koreaTop100Chart = await fetchMelonChart()
+  // const koreaTop100Chart = await fetchMelonChart()
 
-  const koreaTop100ChartList = koreaTop100Chart.response.SONGLIST
+  // const koreaTop100ChartList = koreaTop100Chart.response.SONGLIST
 
-  const newData: {
-    isKoreaChart: true
-    list: MelonChartSong[]
-  } = {
-    isKoreaChart: true,
-    list: koreaTop100ChartList,
-  }
+  // const newData: {
+  //   isKoreaChart: true
+  //   list: MelonChartSong[]
+  // } = {
+  //   isKoreaChart: true,
+  //   list: koreaTop100ChartList,
+  // }
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-col items-center">
       <h1 className="mb-3 text-lg font-medium">국내 TOP 100</h1>
-      <ul className="w-full space-y-2">
+      {/* <ul className="w-full space-y-2">
         {newData.list.map((chart, index) => (
           <Top100ChartList
             key={chart.SONGID}
@@ -30,7 +26,7 @@ const koreaTop100 = async () => {
             index={index}
           />
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
