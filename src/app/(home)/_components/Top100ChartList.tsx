5e87492a638a76db86a27355dee16641f6988539
zzi_eun <je98710@gmail.com>
2025-01-20 22:03:22 +0900

    JL8-26 chore: 바텀시트 오류 수정

--- src/app/(home)/_components/Top100ChartList.tsx
+++ src/app/(home)/_components/Top100ChartList.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import { fetchPlaylistsWithCovers } from '@/api/playlist/actions'
 import MoreOptionsButton from '@/components/common/MoreOptionsButton'
 import { useSpotifySearch } from '@/hooks/useGetSpotifyMusicId'
 import { usePlaylistMusicUpsert } from '@/hooks/usePlaylistMusicUpsert'
+import usePlaylistOperations from '@/hooks/usePlaylistOperations'
 import { useMusicPlayerStore } from '@/store/useMusicPlayerStore'
 import { userStore } from '@/store/userSlice'
-import type { PlaylistRow } from '@/types/playlist'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -44,44 +43,20 @@ const Top100ChartList = ({
   const { user } = userStore((state) => state)
 
   const [isLoading, setIsLoading] = useState(false)
-  const [playlists, setPlaylists] = useState<PlaylistRow[]>([])
+  // const [playlists, setPlaylists] = useState<PlaylistRow[]>([])
 
   const { searchSpotifyId } = useSpotifySearch()
 
   const { isPlayerOpen, setTrackIds, togglePlay, setPlayerOpen } =
     useMusicPlayerStore()
 
-  const getplayList = async () => {
-    setIsLoading(true)
-    try {
-      const data = await fetchPlaylistsWithCovers()
-      setPlaylists(data)
-    } catch (error) {
-      console.error('Error fetching playlists:', error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  const handleMoreOptionBtn = async (
-    musicName: string,
-    artistName: string,
-  ): Promise<playListData> => {
-    await getplayList() // Wait for the playlist to be fetched
-    const data = await searchSpotifyId(musicName, artistName)
-    console.log('searchSpotifyId 71', data)
-
-    if (!data) {
-      throw new Error('Failed to fetch Spotify ID')
-    }
-
-    return data
-  }
+  const { playlists, handleMoreOptionBtn } = usePlaylistOperations()
 
   const { upsertMusic } = usePlaylistMusicUpsert(albumCover)
 
   const handlePlayBtn = async () => {
     // 데이터 일치화를 위해 ()와 안의 텍스트 제거
+
     const newMusicName = musicName.replace(/\s*\(.*?\)\s*/g, '')
     const newArtistiName = artistName.replace(/\s*\(.*?\)\s*/g, '')
 
@@ -89,6 +64,7 @@ const Top100ChartList = ({
 
     await upsertMusic(musicData!)
     const songId = musicData!.id
+    await handleMoreOptionBtn('_', musicName, artistName)
     if (!isPlayerOpen) setPlayerOpen() // 페이지 방문 후, 첫 곡 재생이면 플레이어바 보여줌
     setTrackIds(songId)
     togglePlay()
@@ -126,7 +102,7 @@ const Top100ChartList = ({
         artistName={artistName}
         albumCover={albumCover}
         user={user}
-        onFetchMusicData={() => handleMoreOptionBtn(musicName, artistName)}
+        onFetchMusicData={() => handleMoreOptionBtn('_', musicName, artistName)}
         playlists={playlists}
       />
       {/* </div> */}
