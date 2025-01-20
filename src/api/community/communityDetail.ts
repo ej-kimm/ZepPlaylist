c075fea7fe728da8257c3ba52cabc95bc58ad6d2
bj9322 <bj932218@gmail.com>
2025-01-20 15:34:54 +0900

    git commit -m "JL8-15 feat: 커뮤니티 디테일 페이지에서 필요한 로직 추가"

--- src/api/community/communityDetail.ts
+++ src/api/community/communityDetail.ts
@@ -71,8 +71,48 @@ export const getCommunityDetail = async (
   playlistId: string,
   cookies: string,
 ) => {
+  const supabase = createServerSupabaseClient(cookies)
+
+  const { data: playlistData, error: playlistError } = await supabase
+    .from('playlists')
+    .select(
+      `
+      name,
+      description,
+      playlist_like (
+        user_id
+      ),
+      users:user_id (
+        profile_image,
+        nickname
+      )
+    `,
+    )
+    .eq('id', playlistId)
+    .single()
+
+  if (playlistError) {
+    console.error('Error fetching playlist details:', playlistError.message)
+    throw new Error('플레이리스트 정보를 가져오는 데 실패했습니다.')
+  }
+
+  const likeCount = playlistData.playlist_like.length
+  const user = playlistData.users || {
+    profile_image: null,
+    nickname: 'Anonymous',
+  }
+
   const songs = await getCommunitySongs(playlistId, cookies)
   const comments = await getCommunityComments(playlistId, cookies)
 
-  return { songs, songCount: songs.length, comments }
+  return {
+    songs,
+    songCount: songs.length,
+    comments,
+    playlistName: playlistData.name,
+    description: playlistData.description,
+    likeCount,
+    profileImage: user.profile_image,
+    nickname: user.nickname,
+  }
 }
