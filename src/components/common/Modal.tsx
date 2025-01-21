04d0461e8abc33025ea15b018d21ba6df4e11eb2
Hobin-joo <wnghqls12@naver.com>
2025-01-21 17:46:19 +0900

    JL8-24 feat: 로그인 유도 공용 모달창 스타일 및 박에 클릭시 닫기 설정정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -17,29 +17,33 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   if (!isOpen) return null
   // 성훈님한테 총 크기 어떻게 지정하는지 물어보기
+  // z인덱스 , 랠러티브브
 
   return (
-    <div className="flex h-72 flex-col flex-wrap rounded-[32px] bg-white">
-      <div className="h-72 w-80 rounded-lg bg-white">
-        <h2 className="title-1 text-left">로그인 필요</h2>
-        <p className="button-1 text-left">로그인 화면으로 이동합니다</p>
-        <div className="flex justify-between">
+    <>
+      <div className="fixed left-0 top-0 z-40 h-full w-full flex-col bg-black opacity-50"></div>
+      <div
+        className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center"
+        onClick={onCancel}
+      >
+        <div className="z-50 h-[227px] w-[327px] rounded-[32px] bg-white px-6 pt-[32px]">
+          <h2 className="title-1 text-left">로그인 필요</h2>
+          <p className="button-1 text-left">로그인 화면으로 이동합니다</p>
           <button
             onClick={onCancel}
-            className="button-2 w-full rounded-full bg-secondary text-secondary"
+            className="button-2 mb-[17px] mt-[16px] block h-[39px] w-full rounded-full bg-secondary bg-opacity-10 text-secondary"
           >
             {cancelText}
           </button>
-          <br></br>
           <button
             onClick={onConfirm}
-            className="button-2 w-full rounded-full bg-primary text-white"
+            className="button-2 block h-[39px] w-full rounded-full bg-primary text-white"
           >
             {confirmText}
           </button>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
