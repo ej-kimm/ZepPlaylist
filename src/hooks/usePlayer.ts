db8c2b6e995968ef5e262af5e5bbdc74f913c3da
kej <dossiba0604@gmail.com>
2025-01-21 10:20:59 +0900

    JL8-27 chore : 버셀 오류 테스트

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,5 +1,6 @@
 'use client'
 import { fetchMusicDetailByMusicId } from '@/api/music-play/actions'
+import { getSongLyrics } from '@/api/music-play/genius-api'
 import { fetchPreviewUrl } from '@/api/spotifyToken'
 import { useMusicPlayerStore } from '@/store/useMusicPlayerStore'
 import { useQuery } from '@tanstack/react-query'
@@ -18,15 +19,15 @@ const usePlayer = () => {
       ])
 
       // 노래 가사 가져오기
-      // let lyrics = null
-      // if (musicDetail) {
-      //   lyrics = await getSongLyrics({
-      //     title: musicDetail.title,
-      //     artist: musicDetail.artist,
-      //   })
-      // }
+      let lyrics = null
+      if (musicDetail) {
+        lyrics = await getSongLyrics({
+          title: musicDetail.title,
+          artist: musicDetail.artist,
+        })
+      }
 
-      return { trackUrl, musicDetail }
+      return { trackUrl, musicDetail, lyrics }
     },
     enabled: !!currentTrackId,
     staleTime: 12 * 60 * 60 * 1000, // 12시간
@@ -67,7 +68,7 @@ const usePlayer = () => {
     // isPending,
     musicDetail: musicDetail?.musicDetail,
     url: musicDetail?.trackUrl,
-    // lyrics: musicDetail?.lyrics ?? '😥 제공되는 가사가 없습니다',
+    lyrics: musicDetail?.lyrics ?? '😥 제공되는 가사가 없습니다',
     isPending,
   }
 }
