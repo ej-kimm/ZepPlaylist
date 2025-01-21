0f6da678695bb83ddd7c1ad90c396d1a565fc93b
kej <dossiba0604@gmail.com>
2025-01-21 23:45:38 +0900

    JL8-27 fix : Genius-Lyrics 라이브러리 변경

--- src/app/api/lyrics/route.ts
+++ src/app/api/lyrics/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(request: Request) {
+  // 1. accessToken 키 발급
   const clientId = process.env.GENIUS_CLIENT_ID
   const clientSecret = process.env.GENIUS_CLIENT_SECRET
 
@@ -37,7 +38,30 @@ export async function GET() {
     const tokenData = await tokenResponse.json()
     const accessToken = tokenData.access_token
 
-    return NextResponse.json({ accessToken }, { status: 200 })
+    // 2. 가사 갖고오기
+    const { searchParams } = new URL(request.url)
+    const artist = searchParams.get('artist')
+    const title = searchParams.get('title')
+
+    if (!artist || !title) {
+      return NextResponse.json(
+        {
+          error:
+            'Missing required query parameters: artist, title, or authToken.',
+        },
+        { status: 400 },
+      )
+    }
+
+    const query = `${artist} ${title}`
+
+    const Genius = require('genius-lyrics')
+    const client = new Genius.Client(accessToken)
+    const searches = await client.songs.search(query)
+    const firstSong = searches[0]
+    const lyrics = await firstSong.lyrics()
+
+    return NextResponse.json({ lyrics }, { status: 200 })
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : 'An unknown error occurred.'
--- src/utils/geniusApi.ts
+++ src/utils/geniusApi.ts
@@ -1,15 +1,24 @@
 import { Tables } from '@/types/supabase'
 
-const getAccessToken = async (): Promise<string> => {
+export const getSongLyrics = async ({
+  artist,
+  title,
+}: {
+  artist: Tables<'music'>['artist']
+  title: Tables<'music'>['title']
+}): Promise<string> => {
   try {
-    const response = await fetch('/api/lyrics/token')
+    const response = await fetch(`/api/lyrics?artist=${artist}&title=${title}`)
     if (!response.ok) {
       throw new Error(
         `HTTP Error ${response.status}: Unable to fetch access token.`,
       )
     }
     const data = await response.json()
-    return data.accessToken
+
+    console.log(data.lyrics)
+
+    return data.lyrics
   } catch (error) {
     if (error instanceof Error) {
       console.error(
@@ -22,120 +31,4 @@ const getAccessToken = async (): Promise<string> => {
     }
     throw new Error(`An unexpected error occurred: Please check the logs.`)
   }
-}
-
-const getSongId = async ({
-  artist,
-  title,
-  authToken,
-}: {
-  artist: Tables<'music'>['artist']
-  title: Tables<'music'>['title']
-  authToken: string
-}): Promise<string | null> => {
-  try {
-    const response = await fetch(
-      `/api/lyrics/song-id?artist=${artist}&title=${title}&authToken=${authToken}`,
-    )
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch song ID: ${response.statusText}`)
-    }
-
-    const data = await response.json()
-
-    if (data.error) {
-      console.error(data.error)
-      return null
-    }
-
-    return data.songId
-  } catch (error) {
-    console.error('Error calling getSongId API:', error)
-    return null
-  }
-}
-
-const getLyricsUrl = async (
-  songId: string,
-  authToken: string,
-): Promise<string | null> => {
-  try {
-    const response = await fetch(
-      `/api/lyrics/url?songId=${songId}&authToken=${authToken}`,
-    )
-
-    if (!response.ok) {
-      console.error(`Failed to fetch song details.`)
-      throw new Error(
-        `HTTP Error ${response.status}: Unable to fetch song details.`,
-      )
-    }
-
-    const data = await response.json()
-
-    if (data.error) {
-      console.error(data.error)
-      return null
-    }
-
-    const lyricsUrl = data.lyricsUrl
-    return lyricsUrl
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(
-        'An unknown error occurred while fetching lyrics URL.',
-        error,
-      )
-      throw new Error('An unknown error occurred while fetching lyrics URL.')
-    }
-    throw new Error('An unknown error occurred while fetching lyrics URL.')
-  }
-}
-
-const crawlLyrics = async (lyricsUrl: string): Promise<string> => {
-  try {
-    const response = await fetch(`/api/lyrics/crawl?lyricsUrl=${lyricsUrl}`)
-
-    if (!response.ok) {
-      throw new Error(
-        `HTTP Error ${response.status}: Unable to fetch lyrics page.`,
-      )
-    }
-
-    const data = await response.json()
-
-    if (data.error) {
-      throw new Error(data.error)
-    }
-
-    return data.lyrics
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('An unknown error occurred while crawling lyrics.', error)
-      throw new Error('An unknown error occurred while crawling lyrics.', error)
-    }
-    throw new Error('An unknown error occurred while crawling lyrics.')
-  }
-}
-
-export const getSongLyrics = async ({
-  artist,
-  title,
-}: {
-  artist: Tables<'music'>['artist']
-  title: Tables<'music'>['title']
-}): Promise<string | null> => {
-  const authToken = await getAccessToken()
-  const songId = await getSongId({ artist, title, authToken })
-  if (!songId) {
-    return null
-  }
-  const lyricsUrl = await getLyricsUrl(songId, authToken)
-  if (!lyricsUrl) {
-    return null
-  }
-  const lyrics = await crawlLyrics(lyricsUrl)
-
-  return lyrics
 }
