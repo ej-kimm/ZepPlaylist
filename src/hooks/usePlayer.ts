8ebbb18af11433ac350f148c26495e0128096190
kej <dossiba0604@gmail.com>
2025-01-22 15:49:00 +0900

    JL8-27 chore : usePlayer -> useQueries 주석 처리한 로직 삭제

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -53,38 +53,7 @@ const usePlayer = () => {
     gcTime: 24 * 60 * 60 * 1000, // 24시간
   })
 
-  // const playerQueries = useQueries({
-  //   queries: trackIds.map((trackId) => ({
-  //     queryKey: ['music', trackId],
-  //     queryFn: async () => {
-  //       const [trackUrl, musicDetail] = await Promise.all([
-  //         fetchPreviewUrl(trackId),
-  //         fetchMusicDetailByMusicId(trackId),
-  //       ])
-
-  //       // 노래 가사 가져오기
-  //       let lyrics = null
-  //       if (musicDetail) {
-  //         lyrics = await getSongLyrics({
-  //           title: musicDetail.title,
-  //           artist: musicDetail.artist,
-  //         })
-  //       }
-
-  //       return { trackUrl, musicDetail, lyrics }
-  //     },
-  //   })),
-  // })
-  // const isPending = playerQueries.some(
-  //   (playerQuery) => playerQuery.isLoading || playerQuery.isFetching,
-  // )
-  // const currentTrackData = playerQueries[currentTrackIndex]?.data
-
   return {
-    // musicDetail: currentTrackData?.musicDetail,
-    // url: currentTrackData?.trackUrl,
-    // lyrics: currentTrackData?.lyrics ?? '',
-    // isPending,
     musicDetail: musicDetail?.musicDetail,
     url: musicDetail?.trackUrl,
     lyrics: musicDetail?.lyrics ?? '😥 제공되는 가사가 없습니다',
