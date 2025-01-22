d29ea6958265eabf6e88ac71c58ad26d34607bae
kej <dossiba0604@gmail.com>
2025-01-22 20:57:16 +0900

    JL8-27 style : desktop breakpoint 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      desktop: '720px',
+    },
     extend: {
       colors: {
         primary: '#B15EFF',
