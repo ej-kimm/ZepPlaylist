904ec7975cd8277ccd72bb493694c29a46775b17
Hobin-joo <wnghqls12@naver.com>
2025-01-22 11:05:48 +0900

    JL8-24 style: 회원가입 이용약관 스타일 변경

--- src/app/(auth)/sign-up/_components/SignupForm.tsx
+++ src/app/(auth)/sign-up/_components/SignupForm.tsx
@@ -109,7 +109,7 @@ const SignupForm = () => {
         errorMessage={errors.nickname?.message}
         className="caption-2 mt-6 w-full rounded-lg border-white bg-[#f4f4f4]"
       />
-      <div className="mt-8 flex items-center">
+      <div className="mt-8 flex items-center justify-center">
         <input
           type="checkbox"
           className="mr-2 h-5 w-5 rounded border-gray-300 accent-primary"
