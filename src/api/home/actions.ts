122b20435f2662bfc3d2f225553bf830f223f34a
zzi_eun <je98710@gmail.com>
2025-01-23 00:00:43 +0900

    JL8-26 feat: 최신앨범 디테일 페이지 구현

--- src/api/home/actions.ts
+++ src/api/home/actions.ts
@@ -92,3 +92,26 @@ export const fetchSearchTracks = async (searchParams: string) => {
     throw new Error('An unexpected error occurred')
   }
 }
+
+export const fetchAlbums = async (albumId: string) => {
+  const token = await fetchSpotifyToken()
+
+  try {
+    const res = await fetch(`https://api.spotify.com/v1/albums/${albumId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+    if (!res.ok) {
+      console.error(`API error: ${res.status} ${res.statusText}`)
+      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`)
+    }
+    const data: SpotifyApi.SingleAlbumResponse = await res.json()
+
+    return data
+  } catch (error) {
+    console.error('Fetch error:', error)
+    throw new Error('An unexpected error occurred')
+  }
+}
---
+++ src/app/(home)/latest-album/components/LatestAlbumDetail.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import imPlay from '@/assets/images/imPlay.svg'
+import MoreOptionsButton from '@/components/common/MoreOptionsButton'
+import { useMusicPlayerStore } from '@/store/useMusicPlayerStore'
+import { userStore } from '@/store/userSlice'
+import Image from 'next/image'
+
+type LatestAlbumProps = {
+  albumData: SpotifyApi.SingleAlbumResponse
+}
+
+const LatestAlbumDetail = ({ albumData }: LatestAlbumProps) => {
+  const { user } = userStore((state) => state)
+
+  const { isPlayerOpen, setTrackIds, setPlayerOpen, play } =
+    useMusicPlayerStore()
+
+  const albumTrackData = albumData.tracks.items
+
+  const formatTime = (milliseconds: number): string => {
+    const totalSeconds = Math.floor(milliseconds / 1000)
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = totalSeconds % 60
+    return `${minutes}분 ${seconds.toString().padStart(2, '0')}초`
+  }
+
+  const totalPlayTimeMilliseconds = albumTrackData.reduce(
+    (total, item) => total + item.duration_ms,
+    0,
+  )
+
+  const handlePlayAll = () => {
+    const albumAllTrackIds = albumTrackData.map((song) => song.id)
+    if (!isPlayerOpen) setPlayerOpen()
+    setTrackIds(albumAllTrackIds)
+    play()
+  }
+
+  return (
+    <div>
+      <div>
+        <div className="absolute left-0 top-[56px] w-full">
+          <Image
+            src={albumData.images[0].url}
+            alt={albumData.name}
+            width={375}
+            height={160}
+            className="rounded bg-[#E4E4E4] bg-cover bg-center bg-no-repeat object-cover blur-[5px]"
+            style={{ width: '375%', height: '160px' }}
+          />
+
+          <Image
+            src={albumData.images[0].url}
+            width={248}
+            height={248}
+            alt={albumData.name}
+            priority
+            className="absolute left-1/2 top-[15%] -translate-x-1/2 transform rounded-xl shadow-lg"
+          />
+        </div>
+      </div>
+      <div className="mt-[280px]">
+        <div className="mb-4 flex flex-col items-start">
+          <div className="items-between flex w-[105%] flex-row justify-between">
+            <div className="mb-4">
+              <p className="text-#1B1B1B text-lg font-semibold">
+                {albumData.name}
+              </p>
+              <p className="text-#1B1B1B text-xs font-normal">
+                {albumData.artists[0].name}
+              </p>
+            </div>
+            <button
+              onClick={handlePlayAll}
+              className="flex items-center justify-center rounded-full"
+              style={{
+                width: '48px',
+                height: '48px',
+                padding: '11px',
+                borderRadius: '24px',
+                background: '#9032E8',
+              }}
+            >
+              <Image
+                src={imPlay}
+                alt="전체 재생"
+                width={24}
+                height={24}
+                style={{ flexShrink: 0 }}
+              />
+            </button>
+          </div>
+          <div className="text-#4A4A4A flex items-center justify-center space-x-1 text-[12px] font-normal">
+            <p>곡 수:{albumData.total_tracks}</p>
+            <p>재생 시간: {formatTime(totalPlayTimeMilliseconds)}</p>
+            <p>발매일자: {albumData.release_date}</p>
+          </div>
+        </div>
+      </div>
+      <ul className="space-y-2">
+        {albumTrackData.map((item) => (
+          <li key={item.id} className="flex flex-row items-center">
+            <div className="w-[100%] rounded-lg py-2 transition-colors">
+              <p className="text-#000 text-sm font-semibold">{item.name}</p>
+              <p className="text-sm font-normal text-[rgba(0,0,0,0.60)]">
+                {item.artists[0].name}
+              </p>
+            </div>
+            <MoreOptionsButton
+              musicName={item.name}
+              artistName={item.artists[0].name}
+              albumCover={albumData.images[0].url}
+              user={user}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default LatestAlbumDetail
