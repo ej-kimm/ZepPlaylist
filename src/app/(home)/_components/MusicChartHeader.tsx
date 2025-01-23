a12f7c186544c3ffc5c16623b384129bf05204b4
zzi_eun <je98710@gmail.com>
2025-01-23 22:51:51 +0900

    JL8-26 feat: top100 차트 페이지 현재시각 추가

--- src/app/(home)/_components/MusicChartHeader.tsx
+++ src/app/(home)/_components/MusicChartHeader.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import clock from '@/assets/images/clock.svg'
 import whitePlay from '@/assets/images/whitePlay.svg'
 import { useSpotifySearch } from '@/hooks/useGetSpotifyMusicId'
 import { usePlaylistMusicUpsert } from '@/hooks/usePlaylistMusicUpsert'
@@ -17,13 +18,17 @@ type PlayAllBtnProps = {
 // Vercel Cron Jobs로 1시간 마다 chart 테이블에 데이터를 넣어줘서
 // 한시간에 한번만 api호출하여 데이터를 적재하여 사용하면 호출 비용을 줄일 수 있다.
 // https://www.junetein.com/blog/Next-js-cron-job
-const PlayAllBtn = ({ top100ChartMusic }: PlayAllBtnProps) => {
+const MusicChartHeader = ({ top100ChartMusic }: PlayAllBtnProps) => {
   const { isPlayerOpen, setTrackIds, setPlayerOpen, play } =
     useMusicPlayerStore()
 
   const { searchSpotifyId } = useSpotifySearch()
 
   const { upsertMusic } = usePlaylistMusicUpsert()
+
+  const currentDate = new Date()
+
+  const currentHour = currentDate.getHours()
 
   const handlePlayAll = () => {
     const musicData = top100ChartMusic.map(async (item) => {
@@ -47,17 +52,26 @@ const PlayAllBtn = ({ top100ChartMusic }: PlayAllBtnProps) => {
       })
   }
   return (
-    <button onClick={handlePlayAll} className="mb-3 flex items-center gap-1">
-      <Image
-        src={whitePlay}
-        alt="전체 재생"
-        width={16}
-        height={16}
-        style={{ flexShrink: 0 }}
-      />
-      전체 재생
-    </button>
+    <div className="flex items-center justify-between self-stretch">
+      <button
+        onClick={handlePlayAll}
+        className="mb-3 flex w-[65px] items-center gap-1"
+      >
+        <Image src={whitePlay} alt="전체 재생" width={16} height={16} />
+        <p className="text-xs font-normal">전체 재생</p>
+      </button>
+      <div className="mb-3 flex w-[104px] items-center gap-1">
+        <Image
+          src={clock}
+          alt="현재시각"
+          width={16}
+          height={16}
+          style={{ flexShrink: 0 }}
+        />
+        <p className="text-xs">현재 시각 {currentHour}:00 </p>
+      </div>
+    </div>
   )
 }
 
-export default PlayAllBtn
+export default MusicChartHeader
--- src/app/(home)/billboardTopChart/page.tsx
+++ src/app/(home)/billboardTopChart/page.tsx
@@ -1,7 +1,7 @@
 import { fetchGlobalChart } from '@/api/home/actions'
 import type { BillboradSong } from '@/types/billboradCharts'
+import PlayAllBtn from '../_components/MusicChartHeader'
 import Top100ChartList from '../_components/Top100ChartList'
-import PlayAllBtn from '../_components/playAllBtn'
 
 export const revalidate = 3600
 
@@ -25,22 +25,20 @@ const billboardTop100 = async () => {
     list: billboardTop100ChartList,
   }
   return (
-    <div>
+    <div className="mx-auto flex w-full max-w-3xl flex-col items-center">
       <PlayAllBtn top100ChartMusic={top100ChartMusic} />
-      <div className="mx-auto flex w-full max-w-3xl flex-col items-center">
-        <ul className="w-full space-y-2">
-          {newData.list.map((chart, index) => (
-            <Top100ChartList
-              key={chart.rank}
-              isKoreaChart={newData.isKoreaChart}
-              musicName={chart.title}
-              artistName={chart.artist}
-              albumCover={chart.cover}
-              index={index}
-            />
-          ))}
-        </ul>
-      </div>
+      <ul className="w-full space-y-2">
+        {newData.list.map((chart, index) => (
+          <Top100ChartList
+            key={chart.rank}
+            isKoreaChart={newData.isKoreaChart}
+            musicName={chart.title}
+            artistName={chart.artist}
+            albumCover={chart.cover}
+            index={index}
+          />
+        ))}
+      </ul>
     </div>
   )
 }
