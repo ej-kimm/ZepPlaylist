3c765a98fb272023831661c13060d2b00d6160ce
kej <dossiba0604@gmail.com>
2025-01-24 09:53:34 +0900

    JL8-27 fix : Skeleton 공용 컴포넌트 height 필수 지정값으로 변경

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -1,13 +1,13 @@
 type SkeletonProps = {
+  height: string | number
   width?: string | number
-  height?: string | number
   borderRadius?: string | number
   className?: string
 }
 
 const Skeleton: React.FC<SkeletonProps> = ({
+  height,
   width = '100%',
-  height = '16px',
   borderRadius = '4px',
   className,
 }) => {
