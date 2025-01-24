e59d19847c8e99cbe72da3ba088df70d2e645785
zzi_eun <je98710@gmail.com>
2025-01-24 14:07:59 +0900

    JL8-26 feat: header 최신앨범 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -42,6 +42,8 @@ const Header = () => {
         return '커뮤니티'
       case pathname === '/my-page':
         return '마이 페이지'
+      case pathname.startsWith('/latest-album'):
+        return '최신 앨범'
       default:
         return ''
     }
