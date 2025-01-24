9eb91999da2e84d075954442b9b4edcd64929eb7
bj9322 <bj932218@gmail.com>
2025-01-24 10:25:12 +0900

    git commit -m "JL8-15 feat: 스와이퍼 기능 디테일 추가가"

--- src/app/community/_components/CustomSwiper.tsx
+++ src/app/community/_components/CustomSwiper.tsx
@@ -15,11 +15,19 @@ const CustomSwiper: React.FC<CustomSwiperProps> = ({ items }) => {
       modules={[Autoplay]}
       autoplay={{ delay: 5000, disableOnInteraction: false }}
       breakpoints={{
-        280: { slidesPerView: 1.5, spaceBetween: 8 },
-        380: { slidesPerView: 2.1, spaceBetween: 12 },
-        460: { slidesPerView: 2.5, spaceBetween: 16 },
-        540: { slidesPerView: 3, spaceBetween: 20 },
-        620: { slidesPerView: 3.5, spaceBetween: 24 },
+        280: { slidesPerView: 1.5, spaceBetween: 1 },
+        300: { slidesPerView: 1.7, spaceBetween: 1 },
+        320: { slidesPerView: 1.9, spaceBetween: 1 },
+        340: { slidesPerView: 2, spaceBetween: 2 },
+        380: { slidesPerView: 2.3, spaceBetween: 1 },
+        420: { slidesPerView: 2.5, spaceBetween: 1 },
+        460: { slidesPerView: 2.7, spaceBetween: 1 },
+        500: { slidesPerView: 2.8, spaceBetween: 1 },
+        540: { slidesPerView: 3, spaceBetween: 2 },
+        580: { slidesPerView: 3.2, spaceBetween: 1 },
+        620: { slidesPerView: 3.5, spaceBetween: 1 },
+        660: { slidesPerView: 4, spaceBetween: 1 },
+        700: { slidesPerView: 4, spaceBetween: 1 },
       }}
     >
       {items.map((item) => (
