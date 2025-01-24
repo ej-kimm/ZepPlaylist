9a216c4023d69520b5c8de36be75a79e9bd82704
kej <dossiba0604@gmail.com>
2025-01-24 17:10:15 +0900

    JL8-27 fix : playlist middleware 경로 오류 수정

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -43,8 +43,8 @@ export async function updateSession(request: NextRequest) {
   if (user?.id && (pathname === '/login' || pathname === '/sign-up')) {
     return NextResponse.redirect(new URL('/', request.url))
   }
-  
-  if ((!user?.id && pathname === '/my-page') || pathname === '/playlist') {
+
+  if (!user?.id && (pathname === '/my-page' || pathname === '/playlist')) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
