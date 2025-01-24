5c4424d1683f81b1bff6080052e09c92b5046a3e
kej <dossiba0604@gmail.com>
2025-01-24 14:34:09 +0900

    JL8-27 feat : Modal 공용 컴포넌트 애니메이션 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ModalAnimation from '../animation/ModalAnimation'
 
 interface ModalProps {
   isOpen: boolean
@@ -16,34 +17,24 @@ const Modal: React.FC<ModalProps> = ({
   onCancel,
 }) => {
   if (!isOpen) return null
-  // 성훈님한테 총 크기 어떻게 지정하는지 물어보기
-  // z인덱스 , 랠러티브브
 
   return (
-    <>
-      <div className="fixed left-0 top-0 z-40 h-full w-full flex-col bg-black opacity-50"></div>
-      <div
-        className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center"
+    <ModalAnimation isOpen={isOpen} onClose={onCancel}>
+      <h2 className="title-1 mb-2 text-left">로그인 필요</h2>
+      <p className="button-1 text-left">로그인 화면으로 이동합니다</p>
+      <button
         onClick={onCancel}
+        className="button-2 mb-[17px] mt-[16px] block h-[39px] w-full rounded-full bg-secondary bg-opacity-10 text-secondary"
       >
-        <div className="z-50 h-[227px] w-[327px] rounded-[32px] bg-white px-6 pt-[32px]">
-          <h2 className="title-1 mb-2 text-left">로그인 필요</h2>
-          <p className="button-1 text-left">로그인 화면으로 이동합니다</p>
-          <button
-            onClick={onCancel}
-            className="button-2 mb-[17px] mt-[16px] block h-[39px] w-full rounded-full bg-secondary bg-opacity-10 text-secondary"
-          >
-            {cancelText}
-          </button>
-          <button
-            onClick={onConfirm}
-            className="button-2 block h-[39px] w-full rounded-full bg-primary text-white"
-          >
-            {confirmText}
-          </button>
-        </div>
-      </div>
-    </>
+        {cancelText}
+      </button>
+      <button
+        onClick={onConfirm}
+        className="button-2 block h-[39px] w-full rounded-full bg-primary text-white"
+      >
+        {confirmText}
+      </button>
+    </ModalAnimation>
   )
 }
 
