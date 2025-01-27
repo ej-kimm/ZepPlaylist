5ee0ae7fb42cccd1cabb20e982dfc2437fe11392
kej <dossiba0604@gmail.com>
2025-01-27 13:07:07 +0900

    JL8-61 chore : 가사 번역 api 호출 부분 주석처리

--- src/hooks/useLyricsTranslation.ts
+++ src/hooks/useLyricsTranslation.ts
@@ -1,16 +1,18 @@
-import { getSongTranslate } from '@/api/music-play/lyrics-api'
-import { useQuery } from '@tanstack/react-query'
-
 export const useLyricsTranslation = (lyrics: string) => {
-  const { data: translatedLyrics, isPending } = useQuery({
-    queryKey: ['translate'],
-    queryFn: () =>
-      getSongTranslate({
-        lyrics,
-        targetLang: 'ko',
-      }),
-    enabled: !!lyrics,
-  })
+  // const { data: translatedLyrics, isPending } = useQuery({
+  //   queryKey: ['translate'],
+  //   queryFn: () =>
+  //     getSongTranslate({
+  //       lyrics,
+  //       targetLang: 'ko',
+  //     }),
+  //   enabled: !!lyrics,
+  // })
+
+  // TODO : 발표 당일에 주석 해제
+  const translatedLyrics =
+    '비용이 너무많이들어요.. 발표 당일날 공개합니다.. 돈이없어요'
+  const isPending = false
 
   return { translatedLyrics, isPending }
 }
