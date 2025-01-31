e47ba001c3cf470a3ffd3664f79166ebf47deffb
Hobin-joo <wnghqls12@naver.com>
2025-01-31 14:33:33 +0900

    JL8-24 chore: 안쓰는페이지 주석석

--- src/app/current-playlist/_components/CurrentPageItem.tsx
+++ src/app/current-playlist/_components/CurrentPageItem.tsx
@@ -1,6 +1,6 @@
-const CurrentPageItem = () => {
-  // 여기서 받은 데이타가지고 맵 돌릴거.
-  return <div>{}</div>
-}
+// const CurrentPageItem = () => {
+//   // 여기서 받은 데이타가지고 맵 돌릴거.
+//   return <div>{}</div>
+// }
 
-export default CurrentPageItem
+// export default CurrentPageItem
--- src/app/current-playlist/page.tsx
+++ src/app/current-playlist/page.tsx
@@ -1,16 +1,16 @@
-import CurrentPageItem from './_components/CurrentPageItem'
+// import CurrentPageItem from './_components/CurrentPageItem'
 
-const page = () => {
-  //여기서 데이타 받아와서 프롭스 넘겨주고
-  const data = ['1', '2', '3', '4', '5']
-  return (
-    <div>
-      {data.map((p) => (
-        <CurrentPageItem data={p} />
-      ))}
-    </div>
-  )
-}
+// const page = () => {
+//   //여기서 데이타 받아와서 프롭스 넘겨주고
+//   const data = ['1', '2', '3', '4', '5']
+//   return (
+//     <div>
+//       {data.map((p) => (
+//         <CurrentPageItem data={p} />
+//       ))}
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
 
