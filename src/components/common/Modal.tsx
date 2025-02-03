68472d5c530eee85d5075eb6445ed6a487e20038
kej <dossiba0604@gmail.com>
2025-02-03 21:08:05 +0900

    JL8-103 fix : Modal 공용 컴포넌트 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import React from 'react'
 import ModalAnimation from '../animation/ModalAnimation'
 import { PrimaryButton, SecondaryButton } from './Button'
@@ -7,9 +8,9 @@ interface ModalProps {
   title: string
   content: string | JSX.Element
   type?: 'single' | 'vertical' | 'horizontal' | 'none'
+  className?: string
   onConfirm?: () => void
   onCancel: () => void
-  className?: string
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -17,6 +18,7 @@ const Modal: React.FC<ModalProps> = ({
   title,
   content,
   type = 'single',
+  className,
   onConfirm,
   onCancel,
 }) => {
@@ -64,8 +66,13 @@ const Modal: React.FC<ModalProps> = ({
   }
 
   return (
-    <ModalAnimation isOpen={isOpen} onClose={onCancel}>
-      <div className={`${type === 'horizontal' ? 'mb-6' : 'mb-4'}`}>
+    <ModalAnimation isOpen={isOpen} onClose={onCancel} className={className}>
+      <div
+        className={clsx(
+          'desktop:mb-7',
+          type === 'horizontal' ? 'mb-6' : 'mb-4',
+        )}
+      >
         <h2 className="title-1 mb-2 text-left">{title}</h2>
         <p className="button-1 text-left text-[#4A4A4A]">{content}</p>
       </div>
