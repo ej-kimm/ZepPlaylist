0b8ce8c35f65b9f7853de6a61d39970751efd21a
Hobin-joo <wnghqls12@naver.com>
2025-02-03 20:05:15 +0900

    JL8-24 feat: playlistquery 함수 변경경

--- src/hooks/usePlaylistQuery.ts
+++ src/hooks/usePlaylistQuery.ts
@@ -1,7 +1,7 @@
+import { fetchPlayList } from '@/app/my-page/_components/fetchPlaylist'
 import { fetchUserPlayList } from '@/app/my-page/_components/fetchUserPlayList'
 import { useInfiniteQuery } from '@tanstack/react-query'
-
-export const usePlaylistQuery = () => {
+export const usePlaylistQuery = (playlistType: string) => {
   const {
     data: playlists,
     fetchNextPage,
@@ -11,7 +11,10 @@ export const usePlaylistQuery = () => {
     isLoading,
   } = useInfiniteQuery({
     queryKey: ['playlist'],
-    queryFn: ({ pageParam = 0 }) => fetchUserPlayList({ pageParam }),
+    queryFn: ({ pageParam = 0 }) =>
+      playlistType === 'default'
+        ? fetchUserPlayList({ pageParam })
+        : fetchPlayList({ pageParam }),
     getNextPageParam: (lastPage) => lastPage?.nextCursor || undefined,
     getPreviousPageParam: (firstPage) => firstPage?.prevCursor || undefined,
     initialPageParam: 0,
