ee45a0b6e363422955bc2211fe461daf24773c7e
kej <dossiba0604@gmail.com>
2025-02-04 20:12:40 +0900

    JL8-102 chore : MusicSaveModal 공용 컴포넌트 max-h 변경

--- src/components/common/MusicSaveModal.tsx
+++ src/components/common/MusicSaveModal.tsx
@@ -85,7 +85,7 @@ const MusicSaveModal = ({
               <Skeleton height="16px" className="flex-grow" />
             </div>
           ) : (
-            <ul className="scroll-invisible h-full max-h-[calc(50vh-204px)] space-y-[23px] overflow-y-scroll bg-white">
+            <ul className="scroll-invisible h-full max-h-[280px] space-y-[23px] overflow-y-scroll bg-white">
               {playlists.map((playlist) => (
                 <li
                   key={playlist.id}
