2f8c02e55b4e1f2dd0ceb1c1267b4d922bfcfdee
Hobin-joo <wnghqls12@naver.com>
2025-02-04 19:37:11 +0900

    JL8-24 style: 컨피그 스크린 위치조정 및 모달 스타일일

--- src/app/(auth)/sign-up/_components/SignupForm.tsx
+++ src/app/(auth)/sign-up/_components/SignupForm.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { InputBox, Modal, PrimaryButton } from '@/components/common'
+import BottomSheet from '@/components/common/BottomSheet'
+import useIsDesktop from '@/hooks/useIsDesktop'
 import { supabase } from '@/utils/supabase/client'
 import { zodResolver } from '@hookform/resolvers/zod'
 import clsx from 'clsx'
@@ -14,6 +16,7 @@ const SignupForm = () => {
   const router = useRouter()
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [isChecked, setIsChecked] = useState(false)
+  const isDesktop = useIsDesktop(720)
   const handleOpenModal = () => {
     setIsModalOpen(true)
   }
@@ -150,19 +153,38 @@ const SignupForm = () => {
       <PrimaryButton type="submit" className={clsx('mt-4 h-[39px]')}>
         회원가입
       </PrimaryButton>
-      <Modal
-        isOpen={isModalOpen}
-        title="서비스 이용 약관"
-        content={
-          <Service
-            handleCheckboxChange={handleCheckboxChange}
-            isChecked={isChecked}
+      <>
+        {isDesktop ? (
+          <Modal
+            isOpen={isModalOpen}
+            title="서비스 이용 약관"
+            content={
+              <Service
+                handleCheckboxChange={handleCheckboxChange}
+                isChecked={isChecked}
+              />
+            }
+            onCancel={handleCloseModal}
+            type="none"
+            className={clsx('desktop:h-[580px] desktop:w-[550px]')}
           />
-        }
-        onCancel={handleCloseModal}
-        type="none"
-        className={clsx('desktop:h-[580px] desktop:w-[550px]')}
-      />
+        ) : (
+          <BottomSheet
+            isOpen={isModalOpen}
+            onClose={handleCloseModal}
+            height="80%"
+            maxWidth="100%"
+          >
+            <h1 className="text-[24px]">
+              <b>서비스 이용 약관</b>
+            </h1>
+            <Service
+              handleCheckboxChange={handleCheckboxChange}
+              isChecked={isChecked}
+            />
+          </BottomSheet>
+        )}
+      </>
     </form>
   )
 }
