318e4385e1d08f8d7a06fe237c3c60fccae752d2
bj9322 <bj932218@gmail.com>
2025-02-05 12:53:21 +0900

    git commit -m "JL8-15 feat: page.tsx 코드 변경"

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getCommunityDetail } from '@/api/community/communityDetail'
 import { headers } from 'next/headers'
+import { Suspense } from 'react'
 import CommentSection from '../_components/CommentSection'
+import CommunityDetailSkeleton from '../_components/CommunityDetailSkeleton'
 
 const CommunityDetailPage = async ({
   params,
@@ -9,6 +11,30 @@ const CommunityDetailPage = async ({
 }): Promise<JSX.Element> => {
   const cookies = headers().get('cookie') || ''
   const userId = headers().get('user-id') || ''
+
+  // 데이터를 불러오는 부분
+  const communityDetailPromise = getCommunityDetail(params.id, cookies, userId)
+
+  return (
+    <div>
+      <Suspense fallback={<CommunityDetailSkeleton />}>
+        <CommentSectionWrapper
+          communityDetailPromise={communityDetailPromise}
+          params={params}
+        />
+      </Suspense>
+    </div>
+  )
+}
+
+// CommentSection을 감싸는 컴포넌트
+const CommentSectionWrapper = async ({
+  communityDetailPromise,
+  params,
+}: {
+  communityDetailPromise: ReturnType<typeof getCommunityDetail>
+  params: { id: string }
+}) => {
   const {
     songs,
     comments,
@@ -17,21 +43,19 @@ const CommunityDetailPage = async ({
     profileImage,
     nickname,
     isLiked,
-  } = await getCommunityDetail(params.id, cookies, userId)
+  } = await communityDetailPromise
 
   return (
-    <div>
-      <CommentSection
-        nickname={nickname}
-        profileImage={profileImage}
-        description={description}
-        playlistName={playlistName}
-        songs={songs}
-        comments={comments}
-        playlistId={params.id}
-        isLiked={isLiked}
-      />
-    </div>
+    <CommentSection
+      nickname={nickname}
+      profileImage={profileImage}
+      description={description}
+      playlistName={playlistName}
+      songs={songs}
+      comments={comments}
+      playlistId={params.id}
+      isLiked={isLiked}
+    />
   )
 }
 
