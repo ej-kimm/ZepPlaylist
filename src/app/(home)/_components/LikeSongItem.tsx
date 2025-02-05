3f21137a644553815a425c269cb49096d6fb88c9
kej <dossiba0604@gmail.com>
2025-02-06 00:27:27 +0900

    JL8-108 style : 모바일 화면 홈 페이지 내가 좋아요 한곳 앨범에 gradient 추가

--- src/app/(home)/_components/LikeSongItem.tsx
+++ src/app/(home)/_components/LikeSongItem.tsx
@@ -22,32 +22,69 @@ const LikeSongItem: React.FC<SongItemProps> = ({ item }) => {
     <li className="flex-none" onClick={() => handlePlayBtn(item.spotify_id!)}>
       <div className="h-[120px] w-[120px] text-left">
         <div className="relative">
-          <Image
-            src={item.album_cover!}
-            width={120}
-            height={120}
-            alt={item.title!}
-            priority
-            className="h-auto w-full rounded-2xl shadow-md"
-          />
-          <div className="w-full bg-gradient-to-b from-white via-black">
-            <div className="absolute bottom-1 left-3 mb-1 w-[calc(100%-40px)] transform">
-              <h3 className="caption-1 truncate text-white">{item.title}</h3>
-              <p className="caption-2 truncate text-white text-opacity-80">
-                {item.artist}
-              </p>
-            </div>
+          <div className="relative h-[120px] w-[120px] overflow-hidden rounded-2xl shadow-md">
+            {/* Gradient Background */}
+            <div className="absolute inset-0 bg-[linear-gradient(180deg,rgba(255,255,255,0)_58.33%,rgba(0,0,0,0.45)_79.17%,rgba(0,0,0,0.50)_100%)]" />
 
-            <button
-              className="absolute bottom-[11px] right-[11px] transform"
-              type="button"
-            >
-              <Image src={playing} width={16} height={16} alt={'play'} />
-            </button>
+            {/* 앨범 커버 이미지 */}
+            <Image
+              src={item.album_cover!}
+              width={120}
+              height={120}
+              alt={item.title!}
+              priority
+              className="h-full w-full object-cover"
+            />
           </div>
+
+          {/* 곡 제목, 아티스트 */}
+          <div className="absolute bottom-1 left-3 mb-1 w-[calc(100%-40px)] transform">
+            <h3 className="caption-1 truncate text-white">{item.title}</h3>
+            <p className="caption-2 truncate text-white text-opacity-80">
+              {item.artist}
+            </p>
+          </div>
+
+          {/* 플레이 버튼 */}
+          <button
+            className="absolute bottom-[11px] right-[11px] transform"
+            type="button"
+          >
+            <Image src={playing} width={16} height={16} alt={'play'} />
+          </button>
         </div>
       </div>
     </li>
+
+    // <li className="flex-none" onClick={() => handlePlayBtn(item.spotify_id!)}>
+    //   <div className="h-[120px] w-[120px] text-left">
+    //     <div className="relative">
+    //       <Image
+    //         src={item.album_cover!}
+    //         width={120}
+    //         height={120}
+    //         alt={item.title!}
+    //         priority
+    //         className="h-auto w-full rounded-2xl shadow-md"
+    //       />
+    //       <div className="w-full bg-gradient-to-b from-white via-black">
+    //         <div className="absolute bottom-1 left-3 mb-1 w-[calc(100%-40px)] transform">
+    //           <h3 className="caption-1 truncate text-white">{item.title}</h3>
+    //           <p className="caption-2 truncate text-white text-opacity-80">
+    //             {item.artist}
+    //           </p>
+    //         </div>
+
+    //         <button
+    //           className="absolute bottom-[11px] right-[11px] transform"
+    //           type="button"
+    //         >
+    //           <Image src={playing} width={16} height={16} alt={'play'} />
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </li>
   )
 }
 
