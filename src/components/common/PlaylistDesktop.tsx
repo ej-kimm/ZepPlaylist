b20b29017147efa7c49d03d96a31f2a23b749425
Hobin-joo <wnghqls12@naver.com>
2025-02-05 21:55:04 +0900

    JL8-24 chore: 빈문자열추가가

--- src/components/common/PlaylistDesktop.tsx
+++ src/components/common/PlaylistDesktop.tsx
@@ -30,7 +30,7 @@ const PlaylistDesktopUI = ({
       onClick={onClick}
     >
       <div className="absolute bottom-0 left-0 z-20 h-[40%] w-full bg-gradient-to-b from-transparent via-black/[0.63] to-black/[0.7]"></div>
-      <Image fill alt="최근 앨범사진" src={album_cover} className="z-10" />
+      <Image fill alt="최근 앨범사진" src={album_cover || ''} className="z-10" />
       <div className="absolute bottom-0 z-30 flex w-full items-start justify-between p-2">
         <div>
           <h1 className="text-[#FFFFFF]">{title}</h1>
