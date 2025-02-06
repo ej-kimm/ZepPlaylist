f3e99db9b379e4081eac9b93295b968e961e0dd7
kyh3260 <ppp3260@naver.com>
2025-02-06 17:21:58 +0900

    JL8-20 chore : 오류 수정

--- src/app/playlist/_components/PlaylistDesktop.tsx
+++ src/app/playlist/_components/PlaylistDesktop.tsx
@@ -26,13 +26,10 @@ export default function PlaylistDesktop({
   handleEditPlaylist,
 }: PlaylistDesktopProps) {
   const toggle = useToggleLike()
-<<<<<<< Updated upstream
-=======
   const { user } = userStore()
 
   if (!user) return
 
->>>>>>> Stashed changes
   return (
     <div className="mx-auto max-w-full">
       <h2 className="title-3 mb-24 mt-24 font-pretendard">
