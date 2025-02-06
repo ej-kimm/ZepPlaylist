c52a1a9c7824bc02cd988128d0b16dd5709e07b2
kej <dossiba0604@gmail.com>
2025-02-07 04:25:16 +0900

    JL8-104 fix : 플레이리스트 추가 바텀시트 & 모달 창 여러번 열리는 버그 수정

--- src/components/common/MusicSaveBottomSheet.tsx
+++ src/components/common/MusicSaveBottomSheet.tsx
@@ -5,7 +5,6 @@ import { usePlaylistMusicUpsert } from '@/hooks/usePlaylistMusicUpsert'
 import usePlaylistOperations from '@/hooks/usePlaylistOperations'
 import useScrollLock from '@/hooks/useScrollLock'
 import useSongLike from '@/hooks/useSongLike'
-import { useMusicPlayerStore } from '@/store/useMusicPlayerStore'
 import { userStore } from '@/store/userSlice'
 import type { SpotifyTrack } from '@/types/billboradCharts'
 import Image from 'next/image'
@@ -34,7 +33,6 @@ const MusicSaveBottomSheet = ({
   const { playlists, isPending } = usePlaylistOperations()
   const { upsertMusic, addMusicToPlaylistTable, modal } =
     usePlaylistMusicUpsert()
-  const { closePlayerModal } = useMusicPlayerStore()
   const { songLike, updateLike } = useSongLike({
     user_id,
     spotify_id: musicData?.id,
@@ -61,7 +59,6 @@ const MusicSaveBottomSheet = ({
 
   const handleCloseAllModals = () => {
     modal.closeModal()
-    closePlayerModal()
     handleClose()
   }
 
